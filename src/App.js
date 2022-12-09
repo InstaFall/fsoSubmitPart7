84c180481374684cbeb4b70b4947fe64214d1c29
InstaFall <gorkemcanakilli@hotmail.com>
2022-12-09 22:14:24 +0300

    Commit exercise 7.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useMatch } from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -14,11 +14,23 @@ const Menu = () => {
   )
 }
 
+const Anecdote = ({anecdote}) => (
+  <div>
+    <ul>
+      <li><b>content:</b> {anecdote.content}</li>
+      <li><b>author:</b> {anecdote.author}</li>
+      <li><b>info:</b> {anecdote.info}</li>
+      <li><b>votes:</b> {anecdote.votes}</li>
+      <li><b>id:</b> {anecdote.id}</li>
+    </ul>
+  </div>
+)
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} ><Link to={`/anecdotes/${anecdote.id}`} >{anecdote.content}</Link></li>)}
     </ul>
   </div>
 )
@@ -123,6 +135,10 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match ? anecdotes.find(e => e.id === Number(match.params.id)) : null
+  
+
   return (
     <div>
       <h1>Software anecdotes</h1>
@@ -131,6 +147,7 @@ const App = () => {
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
         <Route path='/create' element={<CreateNew addNew={addNew} />} />
         <Route path='/about' element={<About />} />
+        <Route path='/anecdotes/:id' element={<Anecdote anecdote={anecdote}/>} />
       </Routes>
       <Footer />
     </div>
